d7dc9f4214660035519773b328586e870badca62
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-21 11:10:56 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,7 +1,7 @@
 class CreateShows < ActiveRecord::Migration[5.2]
   def change
     create_table :shows do |t|
-      t.string :title
+      t.string :name
       t.string :day
       t.string :network
       t.integer :rating
